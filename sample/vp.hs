28e4589a5bf342146957c204c0517edd9d4d4fbc
momohatt <momohatt10@gmail.com>
2020-08-03 14:14:02 +0900

    Apply brittany

--- sample/vp.hs
+++ sample/vp.hs
@@ -15,22 +15,17 @@ import           Control.Monad                  ( MonadPlus(..) )
 data MyInteger = MyInteger
 instance Matcher MyInteger Integer
 
-plus10
-  :: Pattern
-       (PP Integer)
-       MyInteger
-       Integer
-       Integer
+plus10 :: Pattern (PP Integer) MyInteger Integer Integer
 plus10 (VP v) _ t = if v + 10 == t then pure v else mzero
-plus10 _ _ t = pure (t - 10)
+plus10 _      _ t = pure (t - 10)
 
 plus10M MyInteger _ = Eql
 
-vpTest n =
-  match dfs n MyInteger
-    [[mc| plus10 #3 -> 100 |],
-     [mc| plus10 $x -> x |],
-     [mc| _ -> 0 |]]
+vpTest n = match
+  dfs
+  n
+  MyInteger
+  [[mc| plus10 #3 -> 100 |], [mc| plus10 $x -> x |], [mc| _ -> 0 |]]
 
 main = do
   print $ show $ vpTest 13 -- return 100
